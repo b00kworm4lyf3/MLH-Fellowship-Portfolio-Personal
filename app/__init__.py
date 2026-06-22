2dc420a8047c857e23c69304ae9cd40207b1ab62
b00kworm4lyf3 <136265146+b00kworm4lyf3@users.noreply.github.com>
2026-06-22 09:41:50 -0600

    nav links added, nothing styled -- going to leave that for issue #11

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 from dotenv import load_dotenv
 
 from .data import hobbies
@@ -7,20 +7,26 @@ from .data import hobbies
 load_dotenv()
 app = Flask(__name__)
 
+LINKS = [{"name": "About",   "endpoint": "about"},
+         {"name": "Work",    "endpoint": "work"},
+         {"name": "Hobbies", "endpoint": "hobbyPage"}]
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    return render_template('index.html', title="MLH Fellow")
 
 @app.route('/hobbies')
 def hobbyPage():
-    return render_template('hobbies.html', title = "My Hobbies", url = os.getenv("URL"), hobbies = hobbies)
+    return render_template('hobbies.html', title = "My Hobbies", hobbies = hobbies)
 
 @app.route('/about')
 def about():
-    return render_template('about.html', title="About Me", url=os.getenv("URL"))
-
+    return render_template('about.html', title="About Me")
 
 @app.route('/work')
 def work():
-    return render_template('work.html', title="Work Experience", url=os.getenv("URL"))
+    return render_template('work.html', title="Work Experience")
+
+@app.context_processor
+def nav():
+    return{"links": LINKS, "url": os.getenv("URL")}
