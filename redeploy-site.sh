2886271c354f8ac03bb16dc51e725840d45ac3a8
b00kworm4lyf3 <136265146+b00kworm4lyf3@users.noreply.github.com>
2026-07-12 19:50:52 -0600

    updated redeploy script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-tmux kill-server
 cd MLH-Fellowship-Portfolio-Personal/
 git fetch && git reset origin/main --hard
 
 source .venv/bin/activate
 pip install -r requirements.txt
 
-tmux new-session -d -s portfolio
-tmux send-keys -t portfolio "cd MLH-Fellowship-Portfolio-Personal/" C-m
-tmux send-keys -t portfolio "source .venv/bin/activate" C-m
-tmux send-keys -t portfolio "flask run --host=0.0.0.0" C-m
+systemctl restart myportfolio
