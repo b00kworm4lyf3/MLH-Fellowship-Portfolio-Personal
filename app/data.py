ce4333ec8358cd0b02660fde0bbd13e06d230251
b00kworm4lyf3 <136265146+b00kworm4lyf3@users.noreply.github.com>
2026-08-21 14:50:45 -0600

    updated personal data

--- app/data.py
+++ app/data.py
@@ -1,80 +1,66 @@
 user = {
-    "name": "Sage Ebert -- deploy workflow change",
+    "name": "Sage Ebert",
     "title": "MLH Fellow - PE Track || Technical Artist",
     "tagline": "Creating at the intersection of art and technology",
     "headshot": "img/SunflowerSage.jpg",
     "altTxt": "Sage smiling at the camera. She is wearing black-rimmed glasses and holding sunflowers.",
     "about": [
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", 
-        "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        "Creative technologist and designer bridging visual storytelling and technical implementation, with a prior career in SFX makeup shaping how I design and build interactive experiences.",
+        "My work spans across digital and physical design, from web development to woodworking, and I most enjoy making things that people connect with and want to touch."
         ],
 }
 
 hobbies = [
-    {"name": "Lorem Ipsum", 
-     "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-     "image": "https://picsum.photos/300/200?grayscale",
-     "altTxt": ""},
-    {"name": "Lorem Ipsum", 
-     "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-     "image": "https://picsum.photos/300/200?grayscale",
-     "altTxt": ""},
-    {"name": "Lorem Ipsum", 
-     "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-     "image": "https://picsum.photos/300/200?grayscale",
-     "altTxt": ""},
-    {"name": "Lorem Ipsum", 
-     "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-     "image": "https://picsum.photos/300/200?grayscale",
-     "altTxt": ""},
-    {"name": "Lorem Ipsum", 
-     "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-     "image": "https://picsum.photos/300/200?grayscale",
-     "altTxt": ""},
+    {"name": "Reading", 
+     "desc": "I enjoy a wide array of genres, though recently I have been reading a lot of horror and urban fantasy. Some of my fav books are Crypt of the Moon Spider by Nathan Ballingrud and Every Heart a Doorway by Seanan McGuire",
+     "image": "img/books.png",
+     "altTxt": "Large library with many books and strange railings."},
+    {"name": "Crafting", 
+     "desc": "I do a lot of crafts. Currently I am crocheting a new sleeve for my laptop and sewing clown pants out of thrifted linen shirts.",
+     "image": "img/mushroom.png",
+     "altTxt": "Crocheted mushroom guy"},
+    {"name": "Gardening", 
+     "desc": "Every year I grow a big garden full of flowers and vegetables, this year I have giant sunflowers!",
+     "image": "img/IMG_3413(1).JPG",
+     "altTxt": "Tall red sunflowers"}
     ]
 
 exp = {
     "current": [
-        {"title": "Position Title",
-         "company": "Company/Org",
+        {"title": "Production Engineering Fellow",
+         "company": "Meta | MLH",
          "year": "2026",
-         "about": ["Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-                   "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-                   "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-                   ]}
+         "about": ["Deployed and maintained a CentOS Linux virtual private server",
+                   "Collaborated with a pod of engineers and Meta mentors to learn about production engineering"
+                   ]},
+        {"title": "Learning Assistant: CS Foundations and Data Structures",
+         "company": "ATLAS Institute - CU Boulder",
+         "year": "2025 - 2026",
+         "about": ["Grade student homework submissions, provide constructive feedback, and help with brainstorming and debugging"
+                   ]},
     ],
     "past": [
-        {"title": "Position Title",
-         "company": "Company/Org",
-         "year": "2020",
-         "about": ["Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-                   "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-                   "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        {"title": "Technical Art Intern",
+         "company": "Reefcraft",
+         "year": "2025",
+         "about": ["Directed development and deployment of the project website, guiding a non-technical teammate through the process", 
+                   "Authored visual design documentation outlining palettes and fonts for use across web and app environments",
+                   "Developed real-time particle and waterflow visualization using custom WGSL shader pipelines",
+                   "Translated static coral growth algorithms into real-time subdividing geometry"
                    ]},
-        {"title": "Position Title",
-         "company": "Company/Org",
-         "year": "2015",
-         "about": ["Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-                   "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-                   "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-                   ]}
     ]
 }
 
 edu = [
-    {"degree": "BS Lorem Ipsum",
-     "school": "Lorem Ipsum University",
-     "period": "2018 - 2022",
-     "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-     "links":[
-            {"link": "#",
-            "linkName": "To Nowhere."},
-            {"link": "#",
-            "linkName": "Also To Nowhere."},]},
-    {"degree": "AA Lorem Ipsum",
-     "school": "Lorem Ipsum Community College",
-     "period": "2016 - 2018",
-     "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    {"degree": "BS Creative Tecnology and Design",
+     "school": "University of Colorado Boulder",
+     "period": "2014 - 2027",
+     "description": "Minor in Computer Science (ABET-accredited program), 4.0 GPA, Tau Sigma Honor Society",
+     "links":[]},
+    {"degree": "Associate General Studies",
+     "school": "Portland Community College",
+     "period": "2022 - 2024",
+     "description": "Phi Theta Kappa Honor Society, 4.0 GPA, Highest Honors",
      "links": None}
 ]
 
@@ -82,23 +68,21 @@ edu = [
 travel = {
     "visited": [
         {
-            "name": "Paris",
-            "year": "2023",
-            "lat": 48.8566,
-            "lon": 2.3522
+            "name": "London",
+            "lat": 51.5074,
+            "lon": 0.1278
         },
         {
-            "name": "Tokyo",
-            "year": "2024",
-            "lat": 35.6762,
-            "lon": 139.6503
+            "name": "Singapore",
+            "lat": 1.2903,
+            "lon": 103.8520
         }
     ],
     "wishlist": [
         {
-            "name": "Bhutan",
-            "lat": 27.5142,
-            "lon": 90.4336
+            "name": "Japan",
+            "lat": 36.2048,
+            "lon": 138.2529
         }
     ]
 }
